2091a87c385a85299bc4c31ca504e0e6b051dd65
Dazled <danielw0703@gmail.com>
2024-08-30 05:04:08 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    const envelope = document.querySelector('.envelope');
-    const dot = document.querySelector('.dot');
-
-    dot.addEventListener('click', function () {
-        envelope.classList.toggle('open');
-    });
-});
