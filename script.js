9c8838ac7e5f27ec8a9eae5a81470febb3c603a5
Dazled <danielw0703@gmail.com>
2024-08-30 05:22:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const envelope = document.querySelector('.envelope');
     const dot = document.querySelector('.dot');
 
-    dot.addEventListener('click', function () {
+    dot.addEventListener('click', function() {
         envelope.classList.toggle('open');
     });
 });
